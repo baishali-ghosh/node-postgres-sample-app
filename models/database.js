cccf7138246a3266a77bcc1889583630405e3222
Baishali Ghosh <baishali13@gmail.com>
2016-04-20 13:43:25 +0530

    Initial commit

---
+++ models/database.js
@@ -0,0 +1,6 @@
+var pg = require('pg');
+var connectionString = '';
+var client = new pg.Client(connectionString);
+client.connect();
+var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+query.on('end', function() {client.end(); });
---
+++ readme.md
@@ -0,0 +1,8 @@
+# PostGre-Node-Todo
+
+A simple app to learn how to build a simple CRUD based todo system in PostGre and Node
+
+## Install
+There are a few different ways you can install:
+
+*Coming Soon
